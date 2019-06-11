e823960561ca3cd7b21da50a5ced6b567071f148
Shelo <YOUR@EMAIL.com>
2019-06-11 17:09:39 +0800

    submit

--- exercise_50/ruby_racer.rb
+++ exercise_50/ruby_racer.rb
@@ -1,10 +1,14 @@
+require "byebug"
 class Die
+  attr_accessor :sides
   def initialize(sides = 6)
+    @sides = sides
   end
 
   # Remember: rolls have to be between 1 and the sides - the minimum you can move is one step!
   # See: http://www.ruby-doc.org/core-2.6.3/Kernel.html#method-i-rand
   def roll
+    rand(1..@sides)
   end
   
 end
@@ -13,34 +17,56 @@ class RubyRacer
 
   # Should hold data about the race:
   # the players, their positions, the track length, etc.
+  attr_accessor :players, :length
   def initialize(players, length = 30)
-  end
-
-  # Returns a random number between the min and max, inclusive
-  def die_roll(min = 1, max = 6)
+    @players = Hash.new{|h,v|h[v]=[]}
+    @length = length
   end
 
   # Returns true if a player has reached the finish line and false otherwise.
   def finished?
+    @players.values.each do |score|
+      if score.sum >= @length
+        return true
+      end
+    end
+    return false
   end
 
   # Rolls the die and advances each player accordingly
   def advance_player!(player)
+    @players[player] << Die.new.roll
   end
 
   # Returns the winner if there is one, nil otherwise
   def winner
+    @players.each do |player,score|
+      if score.sum >= @length
+        return player
+      end
+    end
+    return nil
   end
 
-  # Prints the current game board
+  # Prints the current game boardsss
   # The board should have the same dimensions each time
   def print_gameboard
+    @players.each do |player,score|
+      @length.times do |index|
+        position = score.sum  
+        position > @length-1 ? position = @length-1 : position
+        position == index ? (print " #{player[0,2]}|") : ( print "   |" )
+      end
+      puts
+    end
   end
 
   # For Part 2:
   # Prints the ranking board (Scoreboard)
-  # def print_scoreboard
-  # end
+  def print_scoreboard
+    puts "LEADING BOARD"
+
+  end
 
 end
 
@@ -58,7 +84,7 @@ end
 # Clears the content on the terminal.
 def clear_screen
   print "\e[2J"
-end
+end\
 
 # Moves the insert point in the terminal to the upper left.
 def move_to_home
@@ -68,8 +94,17 @@ end
  #### END HELPER CODE ####
 
  #### DRIVER CODE - DO NOT EDIT BELOW FOR PART 1 ####
+players = []
 
-players = [:a, :b]
+puts "How many players?"
+nums=gets.chomp.to_i
+nums.times do |num|
+  puts "Enter : #{num+1} Player's Name"
+  players << gets.chomp
+  puts 
+end
+puts "And the race begins"
+puts "~~~~~~~~~~~~~~~~~~~"
 
 game = RubyRacer.new(players)
 
@@ -82,7 +117,6 @@ sleep(1)
 # Run the race.
 until game.finished?
   # Do this each round until the race is finished.
-
   players.each do |player|
 
     # Move a player forward.
